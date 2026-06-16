72ef151de0decd2ba9157169172f333c28176d3e
tun43p <tun43p@icloud.com>
2026-06-16 17:24:26 +0200

    chore: create project scaffold

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://jofnu.com',
+  output: 'static',
+  i18n: {
+    locales: ['fr', 'en'],
+    defaultLocale: 'fr',
+    routing: { prefixDefaultLocale: false },
+  },
+  integrations: [mdx(), sitemap()],
+});
